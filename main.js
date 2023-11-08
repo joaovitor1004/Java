2cd74357f64251a524830051726c17d081dc4bfe
joaovitor1004 <108817899+joaovitor1004@users.noreply.github.com>
2023-11-08 11:20:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+// Pom ----------------------------------------
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+listaDeTeclas[0].onclick = tocaSomPom;
